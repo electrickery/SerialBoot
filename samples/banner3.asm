320c2671fd38cb86376ad1e7c8ba555234cd51c2
Fred Jan Kraan <fjkraan@electrickery.nl>
2022-03-27 18:49:03 +0200

    Rudimentary keyboard control test (2 keys).

--- samples/banner3.asm
+++ samples/banner3.asm
@@ -9,7 +9,33 @@
     defb    '***************************'
 
     org     06000h
+    
+    LD      BC, 3C00h
 loop:
+    LD      A, (03820h)
+    BIT     6, A 
+    JP      NZ, NEXTPG
+    BIT     4, A
+    JP      NZ, PREVPG
+    JP      loop 
+        
+NEXTPG:
+    LD      A, '+'
+    LD      (BC), A
+    JP      NXTLOC
+    
+PREVPG:
+    LD      A, '-'
+    LD      (BC), A
+    JP      NXTLOC
+    
+NXTLOC:
+    INC     BC
+    LD      A, C
+    CP      0
+    JR      NZ, not0
+    LD      BC, 3C00h
+not0:
     jp  loop
     
     
